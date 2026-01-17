9d0bf1dec5d189af5b7c57737739c5d1c3ae4228
PARMINDER SOKHAL <parmindersokhal22@gmail.com>
2026-01-17 17:53:07 +0530

    new chnages

--- src/router/userRoutes.js
+++ src/router/userRoutes.js
@@ -5,17 +5,16 @@ import {protect, authorizeRoles } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 // User Registration Route
-router.post('/register', registerUser);
-// router.post('/register', (req, res) => {
-//   return res.status(403).json({
-//     success: false,
-//     message: "Registration is currently disabled",
-//   });
-// });
+// router.post('/register', registerUser);
+router.post('/register', (req, res) => {
+  return res.status(403).json({
+    success: false,
+    message: "Registration is disabled",
+  });
+});
 
 // User Login Route
 router.post('/login', loginUser);
-
 //delete user
 router.delete('/user/:id', protect, authorizeRoles("admin"), deleteUser);
 
